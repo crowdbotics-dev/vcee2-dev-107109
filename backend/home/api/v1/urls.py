d816c8176973d7f3ed04415dd141b43ab001fed1
crowdbotics-dev <engineering@crowdbotics.com>
2023-08-04 10:26:58 +0000

    Update models

--- backend/home/api/v1/urls.py
+++ backend/home/api/v1/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from .viewsets import CderViewSet,CderViewSet,CderViewSet,CderViewSet
 
 from home.api.v1.viewsets import (
     SignupViewSet,
@@ -9,6 +10,7 @@ from home.api.v1.viewsets import (
 router = DefaultRouter()
 router.register("signup", SignupViewSet, basename="signup")
 router.register("login", LoginViewSet, basename="login")
+router.register('cder', CderViewSet )
 
 urlpatterns = [
     path("", include(router.urls)),
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class Cder(models.Model):
+    'Generated Model'
+    ndjdj = models.BigIntegerField()
+    hrrtt = models.BigIntegerField()
